33772893a6b312f7d189ffaf8bfd7a45e5e61f6f
yu <jiayu1922@163.com>
2019-03-11 15:45:00 +0800

    finish Client sending file

--- src/P1/Sever.java
+++ src/P1/Sever.java
@@ -1,10 +1,24 @@
 package P1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Sever {
 	public static void main(String[] args) throws IOException{
+		//--------------------接收Client发送过来的文件流信息------------
 		ServerSocket serverSocket =new ServerSocket(1000);//选定端口1000
+		Socket socket = serverSocket.accept();
+		InputStream inStream = socket.getInputStream();
+		InputStreamReader streamReader =new InputStreamReader(inStream);
+		BufferedReader bufferReader =new BufferedReader(streamReader);
+		String message = "";
+		while((message = bufferReader.readLine())!=null) {
+			System.out.println("Client:"+ message);
+		}
+		socket.shutdownInput();//关闭输入流
 	}
 }
---
+++ src/P2/Scanner.java
@@ -0,0 +1,13 @@
+package P2;
+
+import java.io.IOException;
+
+public class Scanner {
+	public static void main(String[] args) {
+		//利用socket的connect函数进行扫描器的设计，要求有界面
+		//界面能够输入扫描的ip范围和端口范围，和需使用的线程数，显示结果
+		//TODO指定的ip范围
+		//TODO指定端口范围
+		//TODO使用的线程数
+	}
+}
